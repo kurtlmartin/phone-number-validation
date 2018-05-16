4aab6ce426cd5c6de60347356a6f76c86b72e9a0
Kurt Martin <kurt.l.martin@gmail.com>
2018-05-15 20:36:27 -0400

    First commit

---
+++ PhoneNumberValidation/Validation.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PhoneNumberValidation
+{
+    public class Validation
+    {
+        public bool IsPhoneNumberValide(string phoneNumber)
+        {
+            var regexObj = new Regex(@"^(?:(?:\+?[0-9]{1,3}\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$");
+            
+            return regexObj.IsMatch(phoneNumber);
+        }
+    }
+}
---
+++ UnitTest/ValidationTest.cs
@@ -0,0 +1,72 @@
+﻿using PhoneNumberValidation;
+using Xunit;
+
+namespace UnitTest
+{
+    public class ValidationTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void LoadValidation_class()
+        {
+            var newClass = new Validation();
+
+            Assert.NotNull(newClass);
+        }
+
+        [Fact]
+        public void Fail_pass_bad_number()
+        {
+            var validation = new Validation();
+
+            var sut = validation.IsPhoneNumberValide("asdf");
+
+            Assert.False(sut);
+        }
+
+        [Theory]
+        [InlineData("555.555.5555")]
+        [InlineData("555 555 5555")]
+        [InlineData("5555555555")]
+        [InlineData("(555)5555555")]
+        [InlineData("(555)555-5555")]
+        [InlineData("555-555-5555")]
+        public void True_send_valid_number_no_contryCode(string value)
+        {
+            var sut = new Validation().IsPhoneNumberValide(value);
+
+            Assert.True(sut);
+        }
+
+        [Theory]
+        [InlineData("1555.555.5555")]
+        [InlineData("1 555 555 5555")]
+        [InlineData("15555555555")]
+        [InlineData("1(555)5555555")]
+        [InlineData("1(555)555-5555")]
+        [InlineData("1555-555-5555")]
+        public void True_send_valid_number_contryCode_no_Plus(string value)
+        {
+            var sut = new Validation().IsPhoneNumberValide(value);
+            Assert.True(sut);
+        }
+
+        [Theory]
+        [InlineData("+1555.555.5555")]
+        [InlineData("+1555 555 5555")]
+        [InlineData("+15555555555")]
+        [InlineData("+1(555)5555555")]
+        [InlineData("+1(555)555-5555")]
+        [InlineData("+1555-555-5555")]
+        public void True_send_valid_number_contryCode_With_Plus(string value)
+        {
+            var sut = new Validation().IsPhoneNumberValide(value);
+            Assert.True(sut);
+        }
+    }
+}
